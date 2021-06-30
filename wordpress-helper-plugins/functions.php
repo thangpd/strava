9b42307035200d6d10ae8580d259ea74603bbb83
tom <tom@debian.thangpd.info>
2021-06-30 21:02:59 +0700

    add customfiel

--- wordpress-helper-plugins/functions.php
+++ wordpress-helper-plugins/functions.php
@@ -84,3 +84,113 @@ function inspire_get_list_purchased_product_by_user_object( WP_User $user_obj ) 
 
 
 
+// Custom field
+
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_60dbff2342e90',
+		'title' => 'Setting Product Detail',
+		'fields' => array(
+			array(
+				'key' => 'field_60dc2688cda7e',
+				'label' => 'Khoảng cách (km)',
+				'name' => 'distance',
+				'type' => 'number',
+				'instructions' => 'Độ dài thử thách.',
+				'required' => 1,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'min' => '',
+				'max' => '',
+				'step' => '',
+			),
+			array(
+				'key' => 'field_60dc29c029bda',
+				'label' => 'Ngày bắt đầu',
+				'name' => 'start_date',
+				'type' => 'date_picker',
+				'instructions' => 'Chọn ngày bắt đầu',
+				'required' => 1,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'd/m/Y',
+				'return_format' => 'd/m/Y',
+				'first_day' => 1,
+			),
+			array(
+				'key' => 'field_60dc2a0229bdb',
+				'label' => 'Ngày kết thúc',
+				'name' => 'end_date',
+				'type' => 'date_picker',
+				'instructions' => 'Chọn ngày kết thúc.',
+				'required' => 1,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'd/m/Y',
+				'return_format' => 'd/m/Y',
+				'first_day' => 1,
+			),
+			array(
+				'key' => 'field_60dc4123a90eb',
+				'label' => 'Thumbail Thử thách',
+				'name' => 'thumbail_challenge',
+				'type' => 'image',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'return_format' => 'array',
+				'preview_size' => 'large',
+				'library' => 'all',
+				'min_width' => '',
+				'min_height' => '',
+				'min_size' => '',
+				'max_width' => '',
+				'max_height' => '',
+				'max_size' => '',
+				'mime_types' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'product',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+
+endif;
+
+
